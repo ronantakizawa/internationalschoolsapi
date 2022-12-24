6a13157dbe1c62b86832f18f38298a8a08f8021d
ronantakizawa <ronantakizawa@gmail.com>
2022-12-24 13:08:27 +0900

    update2

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const functions = require("./functions");
 const app = express();
 app.use(express.json());
 
-mongoose.connect(process.env.dbURI).then((result)=> app.listen(process.env.PORT,()=>console.log("LIVE")) ).catch((err)=>console.log(err));
+mongoose.connect(process.env.dbURI).then((result)=> app.listen(8000,()=>console.log("LIVE")) ).catch((err)=>console.log(err));
 
 
 
@@ -17,12 +17,7 @@ app.get('/name/:queryname', functions.getByName);
 
 app.get('/:regionname', functions.getByRegion);
 
-//app.get('/:regionname/cap/:capvalue', functions.getByRegionWithCap);
-
 app.get('/:regionname/sortbyfees/:order', functions.getByRegionSortFees);
 
 app.get('/:regionname/sortbynumberofstudents/:order', functions.getByRegionSortNumberofStudents);
 
-//app.get('/:regionname/sortbycurriculum/:curriculumname', functions.getByRegionSortCurriculum);
-
-app.get('/:regionname/findIB', functions.getByRegionFindIB);
